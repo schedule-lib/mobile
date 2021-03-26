dea48324ed70c144cf52b1b186cd2dec01e6694e
eliasall3x <elliasallex@gmail.com>
2021-03-26 18:44:11 -0300

    feat: Timer page componetized

--- src/pages/Timer/index.js
+++ src/pages/Timer/index.js
@@ -1,26 +1,115 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { View, Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, Alert } from "react-native";
+import {
+  Container,
+  Box,
+  HourHitContainer,
+  HourHitGroup,
+  HourHit,
+  HourHitText,
+  Button,
+  ButtonText,
+} from "./styles";
 
 function Timer() {
+  const [hourSelected, setHourSelected] = useState(Number(0));
+  const [isCompleted, setIsCompleted] = useState(true);
+
+  const hours = [
+    { id: 1, hour: "10:30", status: "available" },
+    { id: 2, hour: "11:30", status: "available" },
+    { id: 3, hour: "23:30", status: "available" },
+    { id: 4, hour: "12:30", status: "available" },
+    { id: 5, hour: "15:30", status: "available" },
+    { id: 6, hour: "10:30", status: "unavailable" },
+    { id: 7, hour: "13:30", status: "available" },
+    { id: 8, hour: "10:30", status: "available" },
+    { id: 9, hour: "14:30", status: "unavailable" },
+    { id: 10, hour: "17:30", status: "available" },
+    { id: 11, hour: "18:30", status: "available" },
+    { id: 12, hour: "19:30", status: "unavailable" },
+    { id: 13, hour: "10:30", status: "unavailable" },
+    { id: 14, hour: "10:30", status: "available" },
+    { id: 15, hour: "10:30", status: "unavailable" },
+    { id: 16, hour: "10:30", status: "available" },
+    { id: 17, hour: "10:30", status: "unavailable" },
+    { id: 18, hour: "10:30", status: "available" },
+    { id: 19, hour: "10:30", status: "available" },
+    { id: 20, hour: "10:30", status: "available" },
+  ];
+
+  function handleSelectHour(hour, status) {
+    if (status === "available") {
+      setHourSelected(+hour);
+
+      return 1;
+    }
+    if (status === "unavailable") {
+      setHourSelected(Number(0));
+      Alert.alert("ATENÇAO", "Seleciona apenas HÓRARIO DISPONÍVEL");
+
+      return 0;
+    }
+  }
+  function completeSchedule() {
+    if (hourSelected !== 0) {
+      setIsCompleted(true);
+      Aler.alert("Finalizado", "HORÁRIO RESERVADO");
+
+      return 1;
+    }
+
+    Alert.alert("ATENÇÃO", "SELECIONA UM HORÁRIO DISPONÍVEL");
+    return 0;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Timer</Text>
-    </View>
+    <Container>
+      <Text style={styles.strong}>Servico a ser agendado</Text>
+      <Box styke={styles.box}>
+        <Text style={styles.plainText}>renovar BI</Text>
+      </Box>
+      <Text style={styles.plainText}>Dia 03 de março de 2021</Text>
+      <Text style={styles.plainText}>as 17:30</Text>
+
+      <HourHitContainer>
+        <Text style={styles.strong}>Horários disponíveis</Text>
+
+        <HourHitGroup>
+          {hours.map((item) => (
+            <HourHit
+              key={item.id}
+              status={item.status}
+              onPress={() => handleSelectHour(item.id, item.status)}
+              xClicked={hourSelected}
+              xItem={item.id}
+            >
+              <HourHitText>{item.hour}</HourHitText>
+            </HourHit>
+          ))}
+        </HourHitGroup>
+
+        {isCompleted && (
+          <Button disabled={false} onPress={completeSchedule}>
+            <ButtonText>Escolher horário</ButtonText>
+          </Button>
+        )}
+      </HourHitContainer>
+    </Container>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#312e38",
+  plainText: {
+    fontSize: 18,
+    color: "#121121",
   },
-  title: {
+  strong: {
     fontWeight: "bold",
-    fontSize: 22,
-    color: "#fff",
+    fontSize: 20,
+    color: "#121212",
+    padding: 10,
   },
 });
 
--- src/pages/Timer/styles.js
+++ src/pages/Timer/styles.js
@@ -1,3 +1,61 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View``;
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  background-color: #fff;
+  justify-content: flex-start;
+  /* align-items: center; */
+
+  padding: 10px;
+`;
+
+export const Box = styled.View`
+  border: 1px solid #29292e;
+`;
+
+export const HourHitContainer = styled.View`
+  padding: 10px;
+  width: 99%;
+  margin: 0 auto;
+`;
+
+export const HourHitGroup = styled.View`
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+export const HourHit = styled.TouchableOpacity`
+  width: 60px;
+  height: 50px;
+  border: 0.2px solid rgba(0, 0, 0, 0.5);
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${(props) =>
+    props.status === `unavailable` ? `#ce4a4a` : `#a8a8b3`};
+
+  ${(props) => props.xClicked === props.xItem && `background-color: #04d361`}
+`;
+
+export const HourHitText = styled.Text`
+  font-size: 18px;
+`;
+
+export const Button = styled.TouchableOpacity`
+  display: flex;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: 44px;
+  width: 90%;
+  background-color: ${(props) => (props.disabled ? `#dedede` : `#5929cc`)};
+  border-radius: 6px;
+
+  margin: 15px auto;
+`;
+export const ButtonText = styled.Text`
+  color: #ffffff;
+  font-weight: bold;
+  font-size: 18px;
+`;
